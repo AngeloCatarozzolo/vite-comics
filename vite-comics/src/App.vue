850f6ad1ea9112c6a1f3c2322b666f89a9829c1a
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2022-11-24 13:56:30 +0100

    Start

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import headerVue from './components/header.vue'
+import mainVue from './components/main.vue'
+import footerVue from './components/footer.vue';
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
